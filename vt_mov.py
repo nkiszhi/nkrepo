79448f7b64cd98238f45cbfaf61469829081ecea
Wang Zhi <wztgex@gmail.com>
2019-12-29 20:58:04 +0800

    update vt_mov.py

--- vt_mov.py
+++ vt_mov.py
@@ -11,22 +11,24 @@ REPO = "DATA"
 
 
 def mov_vt_results():
+    n_mov = 0 # The number of moved json files
+    n_del = 0 # The number of deleted json files
     n_json = 0
     json_files = os.listdir(VT_RESULTS)
     for f in json_files:
-        print(f)
-        #print(f[0])
-        #print(f[1])
-        #print(f[2])
-        #print(f[3])
+        if len(f) != 69:
+            continue
         src_path = "{}/{}".format(VT_RESULTS, f)
         dst_path = "{}/{}/{}/{}/{}/{}".format(REPO, f[0], f[1], f[2], f[3], f)
-        #print(src_path)
-        #print(dst_path)
         if not os.path.exists(dst_path):
-            n_json = n_json + 1
+            n_mov = n_mov + 1
             print("{}: {}".format(n_json, f))
             shutil.move(src_path, dst_path)
+        else:
+            n_del = n_del + 1
+            os.remove(src_path)
+    print("[o]: {} json files are stored in repo".format(n_mov))
+    print("[!]: {} duplicated json files are removed.".format(n_del))
 
 def main():
     mov_vt_results()
