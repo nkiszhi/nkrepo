7356b0dfdb84a6ae9d398c180f34abdfb5cad8fa
Zhi Wang <zwang@nankai.edu.cn>
2020-04-17 04:49:15 -0400

    update

--- get_sha256_list.py
+++ get_sha256_list.py
@@ -9,7 +9,6 @@ import os
 hex_string = "0123456789abcdef"
 sample_count = 0
 list_samples = []
-n_json = 0
 
 for i in hex_string:
     for j in hex_string:
@@ -19,20 +18,18 @@ for i in hex_string:
                 list_all = os.listdir(folder)
                 for f in list_all:
                     if len(f) == 64:
-                        f_json = folder + f + ".json"
-                        if os.path.exists(f_json):
-                            n_json = n_json + 1
-                            print("{}: {}".format(n_json, f_json))
-                            continue
-                        else:
-                            list_samples.append(f)
-    f_sha256 = "sha256_{}.txt".format(i)
-    with open(f_sha256, 'w') as f:
-        for item in list_samples:
-            f.write("%s\n" % item)
-    list_samples = []
-    print(f_sha256)
-
+                        list_samples.append(f)
+                        #f_json = folder + f + ".json"
+                        #if os.path.exists(f_json):
+                        #    n_json = n_json + 1
+                        #    print("{}: {}".format(n_json, f_json))
+                        #    continue
+                        #else:
+                        #    list_samples.append(f)
+f_sha256 = "sha256.txt"
+with open(f_sha256, 'w') as f:
+    for item in list_samples:
+        f.write("%s\n" % item)
 
 #print("In total there are " + str(len(list_samples)) + " malware samples without virus total scan result.")
 
