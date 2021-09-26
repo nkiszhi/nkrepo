02fbb11f261bd0fcd967e4c3111ac981f67c0d3a
Wang Zhi <wztgex@gmail.com>
2021-09-26 15:48:06 +0800

    add greet.py to print greeting information

---
+++ control/greet.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "NKAMG"
+__copyright__ = "Copyright (c) 2016 NKAMG"
+__license__ = "GPL"
+
+"""Count the number of all malware samples in the repo."""
+
+import os
+
+def greet():
+    print("\t******************************************")
+    print("\t**                                      **")
+    print("\t**           计算机病毒样本库           **")
+    print("\t**               by NKAMG               **")
+    print("\t**                                      **")
+    print("\t******************************************")
+
+def main():
+    greet()
+
+if __name__ == "__main__":
+    main()
